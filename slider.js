5dd38dc4aa7566dfd04409dd4b5819ab1a339d19
heydi <heydimagali@outlook.com>
2023-05-03 18:34:14 -0300

    agrego menu hamburguesa responsive

--- slider.js
+++ slider.js
@@ -25,3 +25,16 @@ buttons.forEach((button) => {
 });
 
 updateCarousel();
+
+//PAG MONITORES MENU HAMBURGUESA
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('header .navegacion')
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
