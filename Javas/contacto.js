4ca05ffc2c333cef7c3d191d7c2d2c07245d0cb5
Angel2023 <amjel105@gmail.com>
2023-05-12 09:23:23 -0300

    validación de campos en JS

--- Javas/contacto.js
+++ Javas/contacto.js
@@ -7,20 +7,44 @@ const   $submit = document.getElementById('submit'),
 
 // Función !! 
 function validateInputs () {
-        const emailRegex = /^[a-zA-Z0-9/; 
+        //Expresión regular para el email:
+        const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/;
+        
+        //Expresión regular para el asunto (entre 1 y 30 caracteres):
+        const asuntoRegex = /^.{1,30}$/;
+
+        //Expresión regular para el nombre (entre 1 y 20 caracteres):
+        const nombreRegex = /^[a-zA-Z ]{1,50}$/;
+
+        //ALERTAS DE LOS CAMPOS
         if($email.value === "") {
                 alert("Completar el campo de email.");
-                return false;
         }
         if($asunto.value === "") {
                 alert("Completar el campo de asunto.");
-                return false;
         }
         if($nombre.value === "") {
                 alert("Completar el nombre.");
-                return false;
         }
-        return true;
+        
+        //validación de los campos (color rojo!!)
+        //validación nombre:
+        if(!nombreRegex.test($nombre.value)) {
+                alert("Ingrese el formato correcto en el nombre.");
+                $nombre.style.border = "1px solid red";     
+        }
+        
+        //validación email:
+        if(!emailRegex.test($email.value)) {
+                alert("Ingrese el formato correcto en el email.");
+                $email.style.border = "1px solid red";  
+        }
+
+        //validación asunto:
+        if(!asuntoRegex.test($asunto.value)) {
+                alert("Ingrese el formato correcto en el asunto.");
+                $asunto.style.border = "1px solid red";       
+        }
 }
 // Evento !!
 document.addEventListener("click", (e) => {
